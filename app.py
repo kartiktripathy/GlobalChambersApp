0eed9d31fe620200e1483e196be9f88c778fb238
Kartik Tripathi <43439398+kartiktripathy@users.noreply.github.com>
2021-06-17 23:57:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,34 @@ def main():
     dbretrieve()
   if C3.button("Send Mail"):
     send_mail()
-  st.footer("Contact Developer: kartik@globalchamber.org")
+  footer="""<style>
+  a:link , a:visited{
+  color: blue;
+  background-color: transparent;
+  text-decoration: underline;
+  }
+
+  a:hover,  a:active {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+  }
+
+  .footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: white;
+  color: black;
+  text-align: center;
+  }
+  </style>
+  <div class="footer">
+  <p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+  </div>
+  """
+  st.markdown(footer,unsafe_allow_html=True)
 
 if __name__ == '__main__':
   main()
