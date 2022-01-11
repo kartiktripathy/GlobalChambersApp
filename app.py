10b6faa1e01f36d960e6f9c59a611fb11022434a
Kartik Tripathi <43439398+kartiktripathy@users.noreply.github.com>
2022-01-12 01:38:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,11 +57,11 @@ def dbretrieve():
   credentials = ServiceAccountCredentials.from_json_keyfile_name(
       'credentials.json', scope)
   gc = gspread.authorize(gcappversion2-af2507602a8f)
-  spreadsheet_key = '11XSIqD1GDdxYprJ2j5mR5itShgG4lnaONZKaM3Uppvc'
+  spreadsheet_key = '1YOmTVk4co35JFDU3msTknO3aZccdi1ccL2H8sZtjXLY'
   wks_name = 'Master'
   d2g.upload(df, spreadsheet_key, wks_name, credentials=gcappversion2-af2507602a8f, row_names=True)
   #url = 'https://docs.google.com/spreadsheets/d/1YOmTVk4co35JFDU3msTknO3aZccdi1ccL2H8sZtjXLY/edit#gid=279434135'
-  st.header("[Go To Google Sheets](https://docs.google.com/spreadsheets/d/11XSIqD1GDdxYprJ2j5mR5itShgG4lnaONZKaM3Uppvc/edit#gid=0)")
+  st.header("[Go To Google Sheets](https://docs.google.com/spreadsheets/d/1YOmTVk4co35JFDU3msTknO3aZccdi1ccL2H8sZtjXLY/edit#gid=279434135)")
 
 
 def send_mail():
